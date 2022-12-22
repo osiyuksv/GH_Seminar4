538431e3761cbe36782540cc6cda01dc0314ae10
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-22 20:37:47 +0300

    Task1_FSumA ordinary

---
+++ Task1_FSumA/Program.cs
@@ -0,0 +1,12 @@
+﻿// написать функцию / метод, где на вход
+// берём число А, на выход сумма числе от 1 до А
+
+Console.WriteLine("Введите число А");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for (int i = 0; i <= num; i++)
+{
+  sum = sum + i;
+}
+Console.WriteLine(sum);
