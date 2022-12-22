9c786a1a3f1cbeddd44653c36b0b2fb5ddd44591
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-22 23:11:05 +0300

    Task3_Multi

---
+++ Task3_Multi/Program.cs
@@ -0,0 +1,19 @@
+﻿// написать функцию / метод, где на вход
+// берём число А, на выход произведение от 1 до А
+
+Console.WriteLine("Введите число А");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetMultiA());
+
+int GetMultiA()
+{
+    int multi = 1;
+
+    for (int i = 2; i <= num; i++)
+    {
+       multi *= i;
+    }
+    return multi;
+}
+
