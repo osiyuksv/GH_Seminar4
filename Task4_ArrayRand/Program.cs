0f16842b45e62377f1d8578860a465f390eb67a9
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-22 23:32:08 +0300

    Task4_ArrayRand 01

---
+++ Task4_ArrayRand/Program.cs
@@ -0,0 +1,16 @@
+﻿// заполнить массив из 8 элементов
+// в случайном порядке
+
+int[] RandArray()
+{
+    int[] ArrayR = new int[8];
+    for (int i = 0; i < ArrayR.Length; i++)
+    {
+      ArrayR[i] = new Random().Next(0,2); // правое число всегда ставим +1
+      Console.Write(ArrayR[i] + " ");
+    }
+    return ArrayR;
+}
+
+RandArray();
+
