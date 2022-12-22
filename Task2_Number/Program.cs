cb8733bc7a5238c73911c6efb8c32edcf9960dc2
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-22 22:26:27 +0300

    Task2_Number 01

---
+++ Task2_Number/Program.cs
@@ -0,0 +1,19 @@
+﻿// написать функцию / метод, где на вход
+// берём число А, на выход сумма числе от 1 до А
+
+Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int numA()
+{
+    int num = 0;
+    int B = A;
+    for (int i = 0; B > 0; i++)
+    {
+      B = B/10;
+      num = i+1;
+    }
+    return num;
+}
+numA();
+Console.WriteLine(numA());
